2a04067d009ef11d1d66f667a205247a9caf6de0
Christian Treppo <christian@treppo.org>
2019-09-03 18:52:40 +0200

    Use consolidated image for web and dynamic DNS

---
+++ dist/up.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+set -e
+
+sudo addgroup --system --gid 666 web
+sudo adduser --system --disabled-password --no-create-home --uid 666 --shell /sbin/nologin --ingroup web --gecos web web
+sudo chown -R web:web /tmp/nginx/
+
+/home/pi/.local/bin/docker-compose up --detach
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
-#/bin/bash
+#!/usr/bin/env sh
 
 set -ex
 
 ssh pi@192.168.1.128 /home/pi/.local/bin/docker-compose down
-scp -r * pi@192.168.1.128:/home/pi
-ssh pi@192.168.1.128 /home/pi/.local/bin/docker-compose up --detach
+scp -r "$PWD"/dist/* pi@192.168.1.128:/home/pi
+ssh pi@192.168.1.128 /home/pi/up.sh
