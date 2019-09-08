4f7e6c862cac9a0b8cbc8ee001f364c17ebcea99
Christian Treppo <christian@treppo.org>
2019-09-08 18:29:27 +0200

    Always pull latest images on update

--- dist/up.sh
+++ dist/up.sh
@@ -6,4 +6,4 @@ sudo addgroup --system --gid 666 web
 sudo adduser --system --disabled-password --no-create-home --uid 666 --shell /sbin/nologin --ingroup web --gecos web web
 sudo chown -R web:web /tmp/nginx/
 
-/home/pi/.local/bin/docker-compose up --detach
+/home/pi/.local/bin/docker-compose pull && /home/pi/.local/bin/docker-compose up --detach
