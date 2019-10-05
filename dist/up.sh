f1af39a140d8c236c5f94a926ead152114e688cc
Christian Treppo <christian@treppo.org>
2019-10-05 18:22:43 +0200

    Use caddy as web server

--- dist/up.sh
+++ dist/up.sh
@@ -4,6 +4,11 @@ set -e
 
 sudo addgroup --system --gid 666 web
 sudo adduser --system --disabled-password --no-create-home --uid 666 --shell /sbin/nologin --ingroup web --gecos web web
-sudo chown -R web:web /tmp/nginx/
 
-/home/pi/.local/bin/docker-compose pull && /home/pi/.local/bin/docker-compose up --detach
+mkdir -p /mnt/usb/web
+
+cp Caddyfile /mnt/usb/web
+
+PATH=/home/pi/.local/bin:$PATH
+
+docker-compose pull && docker-compose restart || docker-compose up --detach
--- update.sh
+++ update.sh
@@ -2,6 +2,5 @@
 
 set -ex
 
-ssh pi@192.168.1.128 /home/pi/.local/bin/docker-compose down
 scp -r "$PWD"/dist/* pi@192.168.1.128:/home/pi
 ssh pi@192.168.1.128 /home/pi/up.sh
